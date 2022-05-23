710631d8b109e06ad29306adbe448bfb2aaea9e9
danjer9 <103093841+danjer9@users.noreply.github.com>
2022-05-22 20:41:36 -0400

    Update GmailLoginTestCases.java

--- GmailLoginTestCases.java
+++ GmailLoginTestCases.java
@@ -3,6 +3,7 @@ package GmailLogin;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -13,9 +14,9 @@ public class GmailLoginTestCases {
     @Test
     public void login() throws InterruptedException {
 
-        // chromedriver
-        System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver");
-        WebDriver driver = new ChromeDriver();
+        // firefoxdriver
+        System.setProperty("webdriver.gecko.driver","/usr/local/bin/geckodriver");
+        WebDriver driver = new FirefoxDriver();
         driver.get("https://www.gmail.com");
 
         // email
@@ -42,5 +43,37 @@ public class GmailLoginTestCases {
 
         // Exit
         driver.quit();
+        
+        
+        
+        // chromedriver
+        System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver");
+        WebDriver driver1 = new ChromeDriver();
+        driver1.get("https://www.gmail.com");
+
+        // email
+        WebElement useremail = driver1.findElement(By.id("identifierId"));
+        useremail.sendKeys("danjer9@gmail.com",Keys.ENTER);
+        Thread.sleep(4000);
+
+        // pass
+        WebElement password = driver1.findElement(By.name("password"));
+        password.sendKeys("Password",Keys.ENTER);
+        Thread.sleep(4000);
+
+        // Assertion
+        String actualUrl = "https://mail.google.com/mail/u/0/#inbox";
+        String expectedUrl = driver1.getCurrentUrl();
+
+        Assert.assertEquals(expectedUrl,actualUrl);
+
+        if(actualUrl.equalsIgnoreCase(expectedUrl)) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
+
+        // Exit
+        driver1.quit();
     }
 }
